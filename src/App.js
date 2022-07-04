28c8ed9a875d7e8f7037b587b53f8da1ee476563
karansachdeva18 <karan.sachdeva07@gmail.com>
2022-07-04 13:58:43 +0530

    error boundary, layout shift and refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.scss';
+import ErrorBoundary from './components/Common/ErrorBoundary';
 import Weather from './components/Weather/Weather';
 
-
 function App() {
- return(
-   <div className="app">
-   <Weather />
-   </div>
- )
+  return (
+    <div className="app">
+      <ErrorBoundary>
+        <Weather />
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
